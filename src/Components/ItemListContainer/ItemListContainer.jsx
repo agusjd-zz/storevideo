1965dba813c83dbea878a8d24135c3f9067eb76f
Agustin Diaz <agusjd92@gmail.com>
2021-11-29 22:46:54 -0300

    add promises

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { stock } from '../../data/stock'
+
 
 export const ItemListContainer = ({greeting}) => {
+    const [items,setItems]  = useState([])
+    console.log(items)
+
+
+    const pedirDatos = () =>{
+      return new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+           resolve(stock)
+         },2000)
+      })
+    }
+    
+    useEffect(() => {
+      pedirDatos()
+        .then( (resp)=>{
+          setItems(resp)
+
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+        .finally(()=>{
+          console.log("Promesa Finalizada")
+        })
+    }, [])
+  
     return (
         <div>
           {greeting}
---
+++ src/data/stock.js
@@ -0,0 +1,30 @@
+
+export const stock = [
+    {
+      id:1,
+      nombre: "Producto1",
+      precio: 200,
+      img: "https://via.placeholder.com/200",
+      descripcion: "lorem ipsums"
+  
+      
+    },
+    {
+      id:2,
+      nombre: "Producto2",
+      precio: 200,
+      img: "https://via.placeholder.com/200",
+      descripcion: "lorem ipsums"
+  
+      
+    },
+    {
+      id:3,
+      nombre: "Producto3",
+      precio: 200,
+      img: "https://via.placeholder.com/200",
+      descripcion: "lorem ipsums"
+  
+      
+    },
+  ]
